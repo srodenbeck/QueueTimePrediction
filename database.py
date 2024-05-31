41696cdceffc036a93a3e2517289e5bca3c8300d
Austin Lovell <austinlovell@yahoo.com>
2024-05-31 14:09:46 -0400

    Make several changes. I really wish I had the AI commit comment generator thing.

--- database.py
+++ database.py
@@ -1,40 +1,52 @@
 import numpy as np
 import psycopg2
-import psycopg2.extras as extras
 import pandas as pd
 import sqlalchemy
 from psycopg2.extensions import register_adapter, AsIs
-
 import config
 
 register_adapter(np.int64, AsIs)
 
+def strip_post(node_str):
+    if 'G' in node_str:
+        return int(float(node_str[:-1]))
+    else:
+        return int(float(node_str))
 
-def sqlalc(df):
-    db_username = 'postgres'
-    db_password = config.postgres_password
-    db_host = 'slurm-data-loadbalancer.reu-p4.anvilcloud.rcac.purdue.edu'
-    db_port = '5432'
-    db_name = 'sacctdata'
+def sqlalc(df, db_config):
     engine = sqlalchemy.create_engine(
-        f'postgresql+psycopg2://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}')
+        f'postgresql+psycopg2://{db_config["user"]}:{db_config["password"]}@{db_config["host"]}:{db_config["port"]}/{db_config["dbname"]}')
     df.to_sql('jobs', engine, if_exists='append', index=False)
 
 def transform_df(df):
-    df = df[["JobID", "UID", "Account", "State", "Partition", "TimelimitRaw", "Submit", "Eligible", "Planned", "Start",
+    df = df[["JobID", "UID", "Account", "State", "Partition", "TimelimitRaw", "Submit", "Eligible", "PlannedCPURAW", "Start",
              "End",
              "Priority", "ReqCPUS", "ReqMem", "ReqNodes"]]
 
     df = df.rename(columns={"JobID": "job_id", "UID": "user_id", "Account": "account", "State": "state",
                             "Partition": "partition",
                             "TimelimitRaw": "time_limit_raw", "Submit": "submit",
-                            "Eligible": "eligible", "Planned": "planned", "Start": "start_time", "End": "end_time",
+                            "Eligible": "eligible", "PlannedCPURAW": "planned_cpu_raw", "Start": "start_time", "End": "end_time",
                             "Priority": "priority", "ReqCPUS": "req_cpus",
                             "ReqMem": "req_mem", "ReqNodes": "req_nodes"})
+    df['job_id'] = pd.to_numeric(df['job_id'], errors='coerce')
+    df = df.dropna(subset=['job_id'])
+    df['time_limit_raw'] = pd.to_numeric(df['time_limit_raw'], errors='coerce')
+    df = df.dropna(subset=['time_limit_raw'])
     df.loc[df.start_time == "Unknown", "start_time"] = None
     df.loc[df.end_time == "Unknown", "end_time"] = None
-    df['req_mem'] = df['req_mem'].str[:-1]
     df['state'] = df['state'].str.partition(' ')[0]
+
+    df['req_mem'] = df['req_mem'].str[:-1]
+    df['req_mem'] = pd.to_numeric(df['req_mem'], errors='coerce')
+    df = df.dropna(subset=['req_mem'])
+
+    # TODO: Using --units=G appears to break req_nodes column
+    df['req_nodes'] = df['req_nodes'].apply(strip_post)
+
+    partition_enum = ['standard', 'shared', 'wholenode', 'wide', 'gpu', 'highmem', 'azure']
+    df = df[df['partition'].isin(partition_enum)]
+
     df = df.fillna(pd.NA)
     return df
 
@@ -45,7 +57,7 @@ def create_enum(conn):
     command = """DROP TYPE IF EXISTS state_enum CASCADE"""
     with conn.cursor() as cursor: cursor.execute(command)
     command = """CREATE TYPE partition_enum AS ENUM ('standard', 'shared', 'wholenode', 'wide', 'gpu', 'highmem', 
-        'debug', 'gpu-debug')"""
+    'azure')"""
     with conn.cursor() as cursor: cursor.execute(command)
     command = """CREATE TYPE state_enum AS ENUM ('COMPLETED', 'CANCELLED', 'FAILED', 'REQUEUED', 'NODE_FAIL', 'PENDING', 
             'OUT_OF_MEMORY', 'TIMEOUT')"""
@@ -65,7 +77,7 @@ def create_table(conn):
                 time_limit_raw INTEGER,
                 submit TIMESTAMP,
                 eligible TIMESTAMP,
-                planned VARCHAR(255),
+                planned_cpu_raw VARCHAR(255),
                 start_time TIMESTAMP,
                 end_time TIMESTAMP,
                 priority INTEGER,
@@ -75,15 +87,24 @@ def create_table(conn):
                 )"""
     with conn.cursor() as cursor: cursor.execute(command)
 
+def initialize_db(db_config):
+    with psycopg2.connect(dbname=db_config["dbname"], user=db_config["user"], password=db_config["password"], host=db_config["host"],
+                          port=db_config["port"]) as conn:
+        print("Connected to database")
+        create_enum(conn)
+        create_table(conn)
 
-with psycopg2.connect(dbname="sacctdata", user="postgres", password=config.postgres_password, host="slurm-data-loadbalancer.reu-p4.anvilcloud.rcac.purdue.edu",
-                      port="5432") as conn:
-    print("Connected to database")
-    create_enum(conn)
-    create_table(conn)
 
-csv_path = "/home/austin/10min.csv"
-df = pd.read_csv(csv_path, delimiter="|")
-df = transform_df(df)
-
-sqlalc(df)
+if __name__ == "__main__":
+    csv_path = "/home/austin/until_2024-05-01.csv"
+    db_config = {
+        "dbname": "sacctdata",
+        "user": "postgres",
+        "password": config.postgres_password,
+        "host": "slurm-data-loadbalancer.reu-p4.anvilcloud.rcac.purdue.edu",
+        "port": "5432"
+    }
+    initialize_db(db_config)
+    df = pd.read_csv(csv_path, delimiter="|")
+    df = transform_df(df)
+    sqlalc(df, db_config)
---
+++ docs/features.md
@@ -0,0 +1,10 @@
+## List of features used for model
+#### TODO: Put in alphabetical order once finished
+
+- priority - Integer indicating slurm queue priority. Higher indicates job has greater precedence.
+- req_cpus
+- req_mem
+- req_mem
+- count_ahead
+- work_ahead
+- time_limit_raw
