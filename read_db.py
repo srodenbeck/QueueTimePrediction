67789a74975327907817c1a298388e3a4cd207c0
Austin Lovell <austinlovell@yahoo.com>
2024-06-05 14:39:05 -0400

    Calculate_features.py

--- read_db.py
+++ read_db.py
@@ -4,26 +4,16 @@ import sqlalchemy
 import time
 import config
 
-start = time.time()
+def read_to_df():
+        db_config = {
+                "dbname": "sacctdata",
+                "user": "postgres",
+                "password": config.postgres_password,
+                "host": "slurm-data-loadbalancer.reu-p4.anvilcloud.rcac.purdue.edu",
+                "port": "5432"
+        }
 
-
-db_config = {
-        "dbname": "sacctdata",
-        "user": "postgres",
-        "password": config.postgres_password,
-        "host": "slurm-data-loadbalancer.reu-p4.anvilcloud.rcac.purdue.edu",
-        "port": "5432"
-}
-
-engine = sqlalchemy.create_engine(
-        f'postgresql+psycopg2://{db_config["user"]}:{db_config["password"]}@{db_config["host"]}:{db_config["port"]}/{db_config["dbname"]}')
-df = pd.read_sql_query("SELECT * FROM jobs", engine)
-end = time.time()
-print(f"time = {end - start}")
-print(df.head(n=10))
-
-
-arr = df.to_numpy()
-end = time.time()
-print(arr)
-print(f"time = {end - start}")
+        engine = sqlalchemy.create_engine(
+                f'postgresql+psycopg2://{db_config["user"]}:{db_config["password"]}@{db_config["host"]}:{db_config["port"]}/{db_config["dbname"]}')
+        df = pd.read_sql_query("SELECT * FROM jobs ORDER BY RANDOM() LIMIT 10000", engine)
+        return df
