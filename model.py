bbdddb5366699fa541eb247d77ae38f01730e5e5
Austin Lovell <austinlovell@yahoo.com>
2024-06-06 13:55:45 -0400

    Create template for model and training.

---
+++ model.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+
+class nn_model(nn.Module):
+    def __init__(self, input_dim):
+        super(nn_model, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(input_dim, 16),
+            nn.LeakyReLU(),
+            nn.Linear(16, 16),
+            nn.LeakyReLU(),
+            nn.Linear(16, 1)
+        )
+
+    def forward(self, x):
+        out = self.layers(x)
+        return out
