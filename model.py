eccc35751dfb1110ea8c99368a37bcfc605576d7
Austin Lovell <austinlovell@yahoo.com>
2024-06-12 15:06:25 -0400

    Add feature options. Remove unused class

--- model.py
+++ model.py
@@ -67,20 +67,3 @@ class nn_model(nn.Module):
         """
         out = self.model(x)
         return out
-
-class classify_model(nn.Module):
-    def __init__(self, input_dim, hl1, hl2):
-        super(classify_model, self).__init__()
-        layers = [
-            nn.Linear(input_dim, hl1),
-            nn.ReLU(),
-            nn.Linear(hl1, hl2),
-            nn.ReLU(),
-            nn.Linear(hl2, 2)
-        ]
-        self.model = torch.nn.Sequential(*layers)
-
-
-    def forward(self, x):
-        out = self.model(x)
-        return out
