81c0a2c8e455c69b556e5df6671a9e029485fe65
Philip Wisniewski <pwisnie@purdue.edu>
2024-06-17 11:48:12 -0400

    Make file to remove annoying spaces in csv file

---
+++ csv_edit.py
@@ -0,0 +1,16 @@
+import csv
+
+input_file = 'accountNormUsage.csv'
+output_file = 'normUsageDict.csv'
+
+with open(input_file, 'r') as file:
+    lines = file.readlines()
+
+lines = [lines[0]] + [line.lstrip() for line in lines[1:]]
+
+with open(output_file, 'w', newline='') as file:
+    writer = csv.writer(file)
+    for line in lines:
+        writer.writerow(line.strip().split(','))
+
+print(f"Finishing writing file to {output_file}")
